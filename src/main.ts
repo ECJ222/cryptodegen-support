bc3bab4796379435b79f691f51d19487afb141ec
Enoch Chejieh <enochchejieh@gmail.com>
2022-07-22 12:44:50 +0100

    updated robin package

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import router from './router'
 import VueToast from 'vue-toast-notification'
-import RobinChat from 'robin.io-vue2'
+import RobinChat from 'robinapp-vue'
 import App from './App.vue'
 import TitleMixin from './mixins/TitleMixin'
 import './registerServiceWorker'
 import 'vue-toast-notification/dist/theme-sugar.css'
-import 'robin.io-vue2/dist/style.css'
+import 'robinapp-vue/dist/style.css'
 import './assets/styles/global.css'
 
 Vue.config.productionTip = false
