b685c92f235090d7a1efcddc101f82306c9b962b
Enoch Chejieh <enochchejieh@gmail.com>
2022-08-12 11:11:56 +0100

    chore: update pkg name

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import router from './router'
 import VueToast from 'vue-toast-notification'
-import RobinChat from 'robinapp-vue'
+import RobinChat from 'robin-vue'
 import App from './App.vue'
 import TitleMixin from './mixins/TitleMixin'
 import './registerServiceWorker'
 import 'vue-toast-notification/dist/theme-sugar.css'
-import 'robinapp-vue/dist/style.css'
+import 'robin-vue/dist/style.css'
 import './assets/styles/global.css'
 
 Vue.config.productionTip = false
